e0947711ff6e8fcf1a1abd3c540971149ce7487d
野澤優輔 <nozawayuusuke@nozawayuusukenoMacBook.local>
2018-08-20 19:51:01 +0900

    lesson3 end

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'c1b8f325ccae20ab43a6e9d379705f301598c94e83b3613c5936b691368e9a12aeacfab34c452d520b3da139323808de7a38beac42e9d27618159a3731da60e0'
